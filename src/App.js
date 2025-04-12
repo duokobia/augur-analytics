c7176f2e0fd34cbcec3e1faf454007d834eee145
Daniel Okobia <87054930+duokobia@users.noreply.github.com>
2025-04-12 18:28:42 +0100

    feat: Create sidebar component with mockdata to display distributors status

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import React, { useState } from "react";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { distributors } from "./data/mockData";
+
 import "./styles.css";
 
 function App() {
+  const [selectedDistributor, setSelectedDistributor] = useState(
+    distributors[0],
+  );
+
   return (
     <div className="App">
       <Navbar />
       <div className="app-container">
-        <Sidebar />
+        <Sidebar
+          distributors={distributors}
+          selectedDistributor={selectedDistributor}
+          setSelectedDistributor={setSelectedDistributor}
+        />
         <Dashboard />
       </div>
     </div>
